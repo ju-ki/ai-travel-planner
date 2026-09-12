d070e2522d080f07d2ceb3e1214d6f407b124477
ju-ki <juki.programming@gmail.com>
2026-09-12 21:35:13 +0900

    出発地の目的地の移動手段候補が出てこない問題の修正

--- frontend/src/hooks/use-planning.ts
+++ frontend/src/hooks/use-planning.ts
@@ -108,6 +108,8 @@ export const usePlanning = () => {
 
       const result = await executePlanning(params);
 
+      console.log('プランニング結果:', result);
+
       fields.setPlanningResult(date, result);
       fields.setDepartureAndDestination(date, TransportNodeType.DEPARTURE, {
         ...result.updatedDeparture,
